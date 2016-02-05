91d8339118ab4f97735dc13f650a2720e0a1d51d
Pontus Jansson <pontus.jansson@outlook.com>
2016-02-05 14:29:33 +0100

    Add new packages
    Add poststylus, lost-grid and cssnano. Also change gulpfile for the added packages.

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,29 @@ var stylus = require('gulp-stylus');
 var normalize = require('normalize');
 var rupture = require('rupture');
 var typographic = require('typographic');
+var poststylus = require('poststylus');
+var lost = require('lost');
+var notify = require('gulp-notify');
+var nano = require('gulp-cssnano');
 
 gulp.task('styles', function(){
     gulp.src('assets/stylus/app.styl')
         .pipe(stylus({
-            use: [typographic(), rupture(), normalize()]
+            use: [typographic(), rupture(), normalize(), poststylus(['lost'])]
         }))
+        .pipe(gulp.dest('./css/')
+    );
+});
+
+gulp.task('cssnano', function(){
+    gulp.src('css/app.css')
+        .pipe(nano())
+        .pipe(notify("CSS fixed and compiled!"))
         .pipe(gulp.dest('./css/')
     );
 });
 
 gulp.task('watch', function(){
     gulp.watch('assets/stylus/**/*.styl', ['styles']);
+    gulp.watch('css/*.css', ['cssnano']);
 });
